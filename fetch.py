a7f35e200c6800496c8912add7c1a0498953c611
Jonathan <jchiangconan@gmail.com>
2023-07-20 22:22:55 -0400

    added fetching sdf

---
+++ fetch.py
@@ -0,0 +1,25 @@
+# provide methods to get 3D Conformer SDF files from PubChem
+import requests
+
+
+def sdf_from_cid(cid) -> str:
+    """Get 3D Conformer SDF file from PubChem by CID"""
+    url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/" + str(cid) + "/record/SDF/?record_type=3d"
+    response = requests.get(url)
+    return response.text
+
+
+def sdf_from_name(name: str) -> str:
+    """Get 3D Conformer SDF file from PubChem by name"""
+    url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" + name + "/record/SDF/?record_type=3d"
+    response = requests.get(url)
+    return response.text
+
+
+def save_sdf_to_file(sdf: str, filename: str) -> None:
+    with open("molecule_sdf/" + filename, "w") as f:
+        f.write(sdf)
+
+
+if __name__ == "__main__":
+    save_sdf_to_file(sdf_from_cid(2244), "2244.sdf")
